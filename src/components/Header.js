30155947cf5d9ad20f0f960aa1548cbd028d2795
Cannon-Cole <colecannonschool@gmail.com>
2022-04-17 18:51:52 -0600

    Added alt to troll face image

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
     <header className="header">
       <img
         src={require("../images/troll-face.png")}
+        alt="Troll face"
         className="header--image"
       />
       <h2 className="header--title">Meme Generator</h2>
